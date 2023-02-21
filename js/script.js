30d6f8d7699de96c68c129ba61efb53a69f62453
Tyler Kraft <tylerjkraft2002@gmail.com>
2023-02-21 10:02:21 -0500

    broken url auto-removal feature

--- js/script.js
+++ js/script.js
@@ -1,88 +1,159 @@
 /* 
 TODO:
 - Add descriptive tags about the purpose of sites
+    --skim site and search for highly used words (eg. propane, cars)
+    --generate random color tag
+
 - Fix the accuracy of domain sale checker
-
-
+- Add toggle to have broken URLs auto-deleted
+- Better search with tags, prefixes or categories
 */
 
-import wordList from './words.js'
+import wordList from "./words.js";
 
-
-let siteGeneratorTen = document.getElementById("site-generator-ten");
+// let siteGeneratorTen = document.getElementById("site-generator-ten");
 let siteGenerator = document.getElementById("site-generator");
 let siteList = document.getElementById("site-list");
 
 let sites = [];
 
-
 function generateNewSite() {
+  // Word picker, create URL & make link on page
+
   let newSite = wordList[Math.floor(Math.random() * wordList.length)];
   let newSiteUrl = `https://${newSite}.com`;
   let newSiteDiv = document.createElement("div");
   newSiteDiv.innerHTML = `<a target="_blank" href="${newSiteUrl}">${newSite}.com</a>`;
-  newSiteDiv.childNodes[0].classList.add("status-error");
-  newSiteDiv.classList.add("link-backer-error");
 
-  fetch(newSiteUrl).then(function(response) {
-    return response.text();
-  }).then(function(html) {
+  newSiteDiv.childNodes[0].classList.add("status-unknown"); // New default.
+  newSiteDiv.classList.add("link-backer-unknown"); // New default.
 
-    let parser = new DOMParser();
-    let doc = parser.parseFromString(html, 'text/html');
+  // domain-for-sale checker
 
-    let aTagGrabber = doc.querySelectorAll('a');
-    let pTagGrabber = doc.querySelectorAll('p');
+  fetch(newSiteUrl)
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (html) {
+      let parser = new DOMParser();
+      let doc = parser.parseFromString(html, "text/html");
 
-    pTagGrabber.forEach(e => {
-      console.log(e.innerText.trim());
+      let aTagGrabber = doc.querySelectorAll("a");
+      let pTagGrabber = doc.querySelectorAll("p");
+      let h1TagGrabber = doc.querySelectorAll("h1");
+      let h2TagGrabber = doc.querySelectorAll("h2");
+
+      h1TagGrabber.forEach((e) => {
+        let substr = "domain";
+        let lowerCaseE = e.innerText.toLowerCase();
+        if (lowerCaseE.includes(substr)) {
+          newSiteDiv.childNodes[0].classList.add("status-for-sale");
+        }
+      });
+
+      h2TagGrabber.forEach((e) => {
+        let substr = "domain";
+        let lowerCaseE = e.innerText.toLowerCase();
+        if (lowerCaseE.includes(substr)) {
+          newSiteDiv.childNodes[0].classList.add("status-for-sale");
+        }
+      });
+
+      pTagGrabber.forEach((e) => {
+        let substr = "domain";
+        let lowerCaseE = e.innerText.toLowerCase();
+        if (lowerCaseE.includes(substr)) {
+          newSiteDiv.childNodes[0].classList.add("status-for-sale");
+        }
+      });
+
+      aTagGrabber.forEach((e) => {
+        let substr = "domain";
+        let lowerCaseE = e.innerText.toLowerCase();
+        if (lowerCaseE.includes(substr)) {
+          newSiteDiv.childNodes[0].classList.add("status-for-sale");
+        }
+      });
+
+      // begin tag handler ! WORK IN PROGRESS !
+
+      let foundStrings = [];
+
+      pTagGrabber.forEach((e) => {
+        let newPString = e.innerText.toLowerCase().trim();
+        foundStrings.push(newPString);
+      });
+
+      aTagGrabber.forEach((e) => {
+        let newAString = e.innerText.toLowerCase().trim();
+        foundStrings.push(newAString);
+      });
+
+      h1TagGrabber.forEach((e) => {
+        let newH1String = e.innerText.toLowerCase().trim();
+        foundStrings.push(newH1String);
+      });
+
+      h2TagGrabber.forEach((e) => {
+        let newH2String = e.innerText.toLowerCase().trim();
+        foundStrings.push(newH2String);
+      });
+
+      console.log(foundStrings);
     });
 
-    aTagGrabber.forEach(e => {
-      let substr = 'domain'
-      let lowerCaseE = e.innerText.toLowerCase();
-      if(lowerCaseE.includes(substr)) {
-        newSiteDiv.childNodes[0].classList.add("status-for-sale");
-      }
-    });
-    pTagGrabber.forEach(e => {
-      let substr = 'Domain'
-      let substrLower = substr.toLowerCase();
-      if(e.innerText.includes(substr) || e.innerText.includes(substrLower)) {
-        newSiteDiv.childNodes[0].classList.add("status-for-sale");
-      }
-    });
-  }).catch(function(err) {
-    console.warn('Error', err);
-  });
-
+  // -- end main sale tester --
 
   fetch(newSiteUrl, {
     method: "GET",
-  }).then((response) => {
-    console.log(response.url);
-    let responseUrl = response.url;
-    let sellerDomains = [
-      "domain",
-      "dan.com",
-      "squadhelp",
-      "godaddy",
-      "uniregistry",
-    ];
-    if (sellerDomains.some((v) => responseUrl.includes(v))) {
-      newSiteDiv.childNodes[0].classList.add("status-for-sale");
-    }
+  })
+    .then((response) => {
+      // backup sale tester
 
-    if (response.status == 200) {
-      newSiteDiv.childNodes[0].classList.remove("status-error");
-      newSiteDiv.classList.remove("link-backer-error");
-      newSiteDiv.childNodes[0].classList.add("status-success");
-      newSiteDiv.classList.add("link-backer-success")
-    } else {
+      let responseUrl = response.url;
+      let sellerDomains = [
+        "domain",
+        "dan.com",
+        "squadhelp",
+        "godaddy",
+        "uniregistry",
+      ];
+      if (sellerDomains.some((v) => responseUrl.includes(v))) {
+        newSiteDiv.childNodes[0].classList.add("status-for-sale");
+      }
+
+      // status test
+
+      
+      if (response.status == 200) {
+        newSiteDiv.childNodes[0].classList.remove("status-unknown");
+        newSiteDiv.classList.remove("link-backer-unknown");
+        newSiteDiv.childNodes[0].classList.add("status-success");
+        newSiteDiv.classList.add("link-backer-success");
+      } else {
+        newSiteDiv.childNodes[0].classList.remove("status-unknown");
+        newSiteDiv.classList.remove("link-backer-unknown");
+        newSiteDiv.childNodes[0].classList.add("status-error");
+        newSiteDiv.classList.add("link-backer-error");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      newSiteDiv.childNodes[0].classList.remove("status-unknown");
+      newSiteDiv.classList.remove("link-backer-unknown");
       newSiteDiv.childNodes[0].classList.add("status-error");
       newSiteDiv.classList.add("link-backer-error");
-    }
-  });
+
+      let deleteToggle = document.getElementById("delete-toggle");
+      if(deleteToggle.checked == true) {
+        let errorLinks = document.querySelectorAll('.link-backer-error');
+        errorLinks.forEach(e => {
+          e.classList.add("link-removed");
+        })
+
+      }
+
+    });
 
   siteList.appendChild(newSiteDiv);
   sites.push(newSiteUrl);
@@ -90,7 +161,6 @@ function generateNewSite() {
 
 siteGenerator.addEventListener("click", () => {
   generateNewSite();
-  let errorBox = document.getElementById('error-box');
-  errorBox.style.display = 'none';
+  let errorBox = document.getElementById("error-box");
+  errorBox.style.display = "none";
 });
-
